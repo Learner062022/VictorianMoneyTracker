de691795fec219bd9953472353ecc969efdff886
Dylan <ddylan246@yahoo.com>
2024-01-31 19:13:15 +0800

    - Created styles - Created grid layout - Declared row and column definitions - Nested grids

--- DylanDeSouzaVictorianMoneyTracker/DylanDeSouzaVictorianMoneyTracker/MainPage.xaml.cs
+++ DylanDeSouzaVictorianMoneyTracker/DylanDeSouzaVictorianMoneyTracker/MainPage.xaml.cs
@@ -26,5 +26,10 @@ namespace DylanDeSouzaVictorianMoneyTracker
         {
             this.InitializeComponent();
         }
+
+        private void TextBlock_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
